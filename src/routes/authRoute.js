1d7c43df2510e534f75552ef909abdee38fd1aea
prachaya-orr <prachaya.orr@gmail.com>
2022-10-02 14:54:23 +0700

    feature login

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,5 +5,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
+
 
 module.exports = router;

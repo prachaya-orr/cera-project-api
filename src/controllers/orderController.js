641954e5f7f9f65e76b6f93f7f3a72271adf8cda
prachayaOrr <prachaya.orr@gmail.com>
2022-11-02 17:31:57 +0700

    update createOrdere

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,8 +2,9 @@ const { Order } = require('../models');
 
 exports.createOrder = async (req, res, next) => {
   try {
-    const userId = req.user.id;
-    const order = await Order.create(userId);
+    const userId = req.user.id
+    console.log(req.user.id);
+    const order = await Order.create({userId});
     res.status(201).json({ order });
   } catch (err) {
     next(err);
